98eda4f06697be12523fbb0d75c5d4ea46a46220
personaldata143 <62426940+personaldata143@users.noreply.github.com>
2020-03-20 16:25:34 +0300

    Update SampleDS.js

--- SampleDS.js
+++ SampleDS.js
@@ -11,18 +11,16 @@ Follows example at
 https://github.com/mementodatabase/scripts/blob/master/data-sources/discogs.js
 */
 
-var baseurl = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"
+var baseurl = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
 
 function SampleDS (apiKey) {
     this.apiKey = "20092269-c22f-46c2-a8c0-3bf0c4a9f097";
 }
 
 SampleDS.prototype.search = function(q) {
-  var resultJson = http().get(baseurl + encodeURIComponent(q) + "?key=" + this.apikey );
-  var res = JSON.parse(resultJson.body);
-  var result = {};
-    
 
+  var result = {};
+   
  
   return result;
 
